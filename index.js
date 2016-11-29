5cd5b816eb80d9c8a6f50a2b8b39cd4233531c38
Richard Porczak <rporczak@gmail.com>
2016-11-28 22:27:14 -0800

    Manually runs through once successfully. Happy path.

--- index.js
+++ index.js
@@ -8,48 +8,22 @@ var rest    = new Client();
 var Twit    = require('twit');
 var T       = new Twit(keys.twitter);
 
-
 var args = {
   headers: {
-    "User-Agent": "@TodayInGB -- Twitter bot. A handful of requests once every few hours.",
+    "User-Agent": "@TodayInGB -- Twitter bot. A handful /videos/ of requests once every couple hours.",
     "Content-Type": "application/json"
   }
 };
 
 exists = function (x) { return (typeof x !== "undefined" && x !== null); };
 
-dmError = function (error) {
-  dm = {
-    'screen_name': 'tsiro',
-    'text': ('ERROR: "' + error + '"')
-  };
-  T.post('direct_messages/new', dm, function(err, reply) {
-    if (err) {
-      console.log('error:', err);
-    }
-  });
-};
+twoHoursIsh = function () {
+  var TWO_HOURS       = 1000 * 60 * 60 * 2;
+  var THIRTY_MINUTES  = 1000 * 60 * 30;
+  var WIGGLE_ROOM     =
+    Math.floor(2 * THIRTY_MINUTES * Math.random()) - THIRTY_MINUTES;
 
-getVideosURL = function (year) {
-  var url = encodeURI(
-    'http://www.giantbomb.com/api/videos/?' +
-      'api_key=' + keys.gb +
-      '&field_list=name,deck,id,publish_date,site_detail_url' +
-      '&filter=publish_date:' + getTodayRange(year) +
-      '&format=json'
-  );
-
-  return url;
-};
-
-getTodayRange = function (year) {
-  var start, end;
-  var today = Date.now();
-
-  start = year + moment(Date.now()).startOf('day').format("-MM-DD") + " 00:00:00";
-  end   = year + moment(Date.now()).endOf('day').format("-MM-DD")   + " 23:59:59";
-
-  return start + "|" + end;
+  return TWO_HOURS + WIGGLE_ROOM;
 };
 
 getYears = function () {
@@ -62,6 +36,25 @@ getYears = function () {
   }
 
   return years;
+};
+
+getVideosURL = function (year) {
+  var start, end;
+  var today = Date.now();
+
+  start = year + moment(Date.now()).startOf('day').format("-MM-DD") + " 00:00:00";
+  end   = year + moment(Date.now()).endOf('day').format("-MM-DD")   + " 23:59:59";
+
+  var yearRange = start + "|" + end;
+  var url = encodeURI(
+    'http://www.giantbomb.com/api/videos/?' +
+      'api_key=' + keys.gb +
+      '&field_list=name,deck,id,publish_date,site_detail_url' +
+      '&filter=publish_date:' + yearRange +
+      '&format=json'
+  );
+
+  return url;
 };
 
 fetchVideos = function (URLs, videos, cb) {
@@ -101,7 +94,7 @@ pickVideo = function (videos, timeBias) {
   return videos[index];
 };
 
-entry = function () {
+getTwitterStatus = function (cb) {
   var random = new Random(
     Random.engines.mt19937().seed(moment().format("MMMM DD YYYY")));
 
@@ -122,31 +115,73 @@ entry = function () {
   }
 
   fetchVideos(URLs, [], function (results) {
-    // console.log("results: ");
-    // console.log(random.shuffle(results));
-
-    // shuffle videos
     random.shuffle(results);
     var video = pickVideo(results, timeBias);
 
-    var year    = moment(video["publish_date"]).year();
-    var status  = video.name + " (" + year + ") " + video["site_detail_url"];
-    var params  = { 'status': status };
+    var date    = moment(video["publish_date"]).format("MMM DD, YYYY");
+    var status  = video.name + " (" + date + ") " + video["site_detail_url"];
 
-    console.log(status);
+    var out = {
+      error:  null,
+      name:   video.name,
+      date:   date,
+      status: status
+    };
 
-    // T.post(
-    //   'statuses/update',
-    //   params,
-    //   function (err, data, response) {
-    //     if (!err) {
-    //       console.log("Success!");
-    //     }
-    //   })
-
-
+    cb(out);
   });
-
 };
 
-entry();
+handleError = function (error) {
+  dm = {
+    'screen_name': 'tsiro',
+    'text': ('ERROR: "' + error + '"')
+  };
+
+  T.post('direct_messages/new', dm, function(err, reply) {
+    if (err) {
+      console.log('error:', err);
+    }
+  });
+
+  setTimeout(theHat, twoHoursIsh());
+};
+
+theHat = function () {
+  T.get(
+    'users/search', { "q":"TodayInGB" },
+    function (err, data, response) {
+      if (!exists(err) && exists(data) && exists(data[0])) {
+        var screen_name = data[0].screen_name;
+        var lastStatus  = data[0].status;
+
+        if (screen_name === "TodayInGB" && exists(lastStatus)) {
+          var status_text = lastStatus.text;
+
+          getTwitterStatus(function(data) {
+            var error   = data.error;
+            var name    = data.name;
+            var date    = data.date;
+            var status  = data.status;
+
+            var safeName = name.slice(0, (139 - 4 - date.length));
+
+            if (status_text.indexOf(safeName + " (" + date + ")") === -1) {
+              T.post(
+                'statuses/update', { 'status': status },
+                function (err, data, response) {
+                  if (!err) {
+                    console.log("Success!");
+                  }
+                  // setTimeout(theHat, twoHoursIsh());
+                }
+              );
+            }
+          });
+        }
+      }
+    }
+  )
+};
+
+theHat();
